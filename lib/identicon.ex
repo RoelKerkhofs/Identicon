b926f9dfadf0e1c4acd9a2eaa738da290aa430d8
Roel Kerkhofs <roel@Roels-MacBook-Air.local>
2018-12-24 11:47:57 +0100

    Lecture 42 - Chunking into lists of 3

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -6,6 +6,13 @@ def main(input) do
     input
     |> hash_input
     |> pick_color
+    |>build_grid
+end
+
+def build_grid(%Identicon.Image{hex: hex} = image) do
+  hex
+  |> Enum.chunk_every(3)
+  #|> mirror_row
 end
 
 def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
