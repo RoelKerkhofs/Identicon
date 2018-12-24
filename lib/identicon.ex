6adad0fac379d297892f36a9b3c34544a2e19c3b
Roel Kerkhofs <roel@Roels-MacBook-Air.local>
2018-12-24 13:11:02 +0100

    Lecture 46 - Filter odds

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -7,6 +7,12 @@ def main(input) do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
+end
+
+def filter_odd_squares(%Identicon.Image{grid: grid}) do
+  Enum.filter grid, fn({code, _index}) ->
+    rem(code, 2) == 0 end
 end
 
 def build_grid(%Identicon.Image{hex: hex} = image) do
