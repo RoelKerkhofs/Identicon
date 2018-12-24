70477a84f159de91fbbb7b173c33a0b6eeef4f1c
Roel Kerkhofs <roel@Roels-MacBook-Air.local>
2018-12-24 14:26:06 +0100

    Lecture 49 - drawing image and save

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -9,6 +9,23 @@ def main(input) do
     |> build_grid
     |> filter_odd_squares
     |> build_pixel_map
+    |> draw_image
+    |> save_image (input)
+end
+
+def save_image(image, input) do
+  File.write("#{filename}.png", image)
+end
+
+def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+  image = :edg.create(250, 250)
+  fill = :edg.color(color)
+
+  Enum.each pixel_map fn({start, stop}) ->
+    :edg.filledRectangle(image, start, stop, fill)
+  end
+
+  :edg.render(image)
 end
 
 def build_pixel_map (%Identicon.Image{grid: grid} = image) do
